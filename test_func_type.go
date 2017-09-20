cddd6b287d6e6772f4d550bdf1926c6f29422faf
Xianyu <hpxianyu.xianyu@wearapay.com>
2017-09-20 11:16:12 +0800

    [+] test func type

---
+++ test_func_type.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Sfun func(a *int)
+
+func Test(a *int) {
+    fmt.Printf("%03d\n",*a)
+}
+
+func Test1(a *int , b *int) {
+    fmt.Printf("%04d : %05d\n", *a, *b)
+}
+
+func main() {
+    num := 4234234
+    dd := Sfun(Test)
+
+    dd(&num)
+
+
+}
