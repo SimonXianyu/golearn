0c631b42961df84c351b1729c46a79b2c97fc78b
Xianyu <hpxianyu.xianyu@wearapay.com>
2017-09-22 16:35:27 +0800

    [+] add test for struct methods

--- mytest/mytest.go
+++ mytest/mytest.go
@@ -11,4 +11,32 @@ type MyTestController struct {
 
 func init() {
     fmt.Println("myTest init executed")
+
+    fmt.Println(" MyTestController ------")
+    var c1 MyTestController
+    c1.doSome()
+    c1.doSome2()
+
+    fmt.Println(" MyTest2Controller ------")
+    var c2 MyTest2Controller
+    c2.doSome()
+    c2.doSome2()
+    fmt.Println(" MyTest2Controller as MyTestController ------")
+    c2.MyTestController.doSome()
+}
+
+func (*MyTestController) doSome() {
+    fmt.Println("Test1 Do Some")
+}
+
+func (*MyTestController) doSome2() {
+    fmt.Println("Test1 Do Some 2")
+}
+
+type MyTest2Controller struct {
+    MyTestController
+}
+
+func (*MyTest2Controller) doSome() {
+    fmt.Println("Test2 Do some")
 }
